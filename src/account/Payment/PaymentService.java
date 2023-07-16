5b34855fce9e4e4ee02d0ed4be3b25baec065c56
Denis Stoyanouski <vandenst@tut.by>
2023-07-16 17:15:40 +0300

    added PaymentService and PaymentRepository classes

--- src/account/Payment/PaymentService.java
+++ src/account/Payment/PaymentService.java
@@ -1,5 +1,16 @@
 package account.Payment;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PaymentService {
+
+    @Autowired
+    PaymentRepository paymentRepository;
+
+    public void addPayment(Payment payment) {
+        paymentRepository.save(payment);
+    }
+
 }
